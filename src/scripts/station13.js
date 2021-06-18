e261498e9e612d062cfa0561231733f6e53607bf
egublog <egukei0729@gmail.com>
2021-06-18 23:37:47 +0900

    station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,5 +1,6 @@
 function getData() {
-    return arrowFunction()
+    // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
+    const arrow = () => {return 'Arrow'};
+    
+    return arrow();
 }
-
-// Arrowという文字列を返すarrowFunctionという名前のアロー関数を追加してください
